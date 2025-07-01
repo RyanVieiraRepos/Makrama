8861783252d2abe7b54650a7f8371ce51b6aba68
ryanviera05 <ryan.assumpcao@outlook.com>
2025-06-30 22:08:22 -0300

    test

---
+++ Footer and Header/links.js
@@ -0,0 +1,18 @@
+let login = document.getElementById("loginContainer")
+
+login.addEventListener('click', function redirect() {
+    window.location.href = '../Pages/login/login.html'
+});
+
+
+//whatsapp redirect
+let whats = document.getElementById("whats")
+whats.addEventListener('click', function redirect() {
+    window.location.href='https://web.whatsapp.com'
+})
+
+//insta redirect
+ let insta = document.getElementById("insta")
+insta.addEventListener('click', function redirect() {
+    window.location.href='https://www.instagram.com/'
+})
