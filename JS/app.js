38bc37936ee29246d977418cd4f2a3544f53c25b
ryanviera05 <ryan.assumpcao@outlook.com>
2025-07-01 22:23:44 -0300

    Footer

--- JS/app.js
+++ JS/app.js
@@ -2,6 +2,12 @@
         let product = document.getElementById("product")
 
         product.addEventListener('click', function redirect() {
-            window.location.href='../Pages/produtos/produto1/produto1.html'
+            window.location.href='https://ryanvieirarepos.github.io/Makrama/Pages/produtos/produto1/produto1.html'
         })
 
+//as bibliotecas front end, agrupam linhas de código em um só termo
+// já as frameworks substituem o html e css por exemplo
+
+//As frameworks tomam conta do projeto, da sua estrutura como um todo?       //verdade
+//bootstrap é um framework ?   //não ela apenas disponibiliza componentes a serem usades  
+//react é uma biblioteca ? falsa
