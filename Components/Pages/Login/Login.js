ac533d3e4a06ba14d62eeb10b19cfceacafc489e
ryanviera05 <ryanvieiramail@gmail.com>
2025-10-16 13:35:53 -0300

    login.csss

--- Components/Pages/Login/Login.js
+++ Components/Pages/Login/Login.js
@@ -1,11 +1,9 @@
 class Login extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <div class="main2">
-                <div class="mainLogin2">
-                    <strong class="titleStrong2">Login</strong>
+            <form class="loginForm" action="/">
+                        <strong class="loginTitle">Login</strong>
 
-                    <form classN="form2" action="/">
                         <input
                             type="text"
                             id="email2"
@@ -14,29 +12,25 @@ class Login extends HTMLElement {
                             placeholder="E-Mail"
                             required
                         />
-                        <div class="line2"></div>
+                        
 
                         <input
                             type="password"
-                            id="senha2"
-                            name="senha2"
-                            className="input12"
+                            id=""
+                            name=""
+                            
                             placeholder="Senha"
                             required
                         />
-                        <div class="line2"></div>
+                        
 
-                        <div class="loginButton2" id="loginButton2">
-                            <div class="spacer2"></div>
-                            <strong class="buttonStrong2">Login</strong>
+                        <div class="loginButtons">
+                            <strong>Login</strong>
+                            <strong>Não possui uma conta ?</strong>
                         </div>
 
-                        <div class="cont2">
-                            <Link to='/Cadastro' class="signin2"><strong>Não possui uma conta ?</strong></Link>
-                        </div>
+                        
                     </form>
-                </div >
-            </div >
     `
     }
 } customElements.define('main-login', Login)
